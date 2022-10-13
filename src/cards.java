7cf05788e553dd1accc39cc0f34a8d6c3afd7e13
User <User@100.84.255.4>
2022-10-13 15:55:58 +0100

    cards

---
+++ src/cards.java
@@ -0,0 +1,23 @@
+
+public class cards {
+
+
+	public static void main(String[] args) {
+	
+	   String xd = "┌─────────┐", xdd = "│ K       │";
+	
+	   
+	   System.out.println(xd);
+	   System.out.println(xdd);
+	   
+	}
+	
+}
+//┌─────────┐
+//│ K       │
+//│         │
+//│         │
+//│         │
+///│         │
+//│         │
+//└─────────┘
